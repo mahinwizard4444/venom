68959ba9186e8f1278bb0dcf4a69f3e14a90aa12
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2022-02-13 10:28:22 +0530

    Create purge.py

---
+++ plugins/purge.py
@@ -0,0 +1,42 @@
+from pyrogram import Client as app
+from plugins.helper.permissions import adminsOnly
+import os
+import asyncio
+from pyrogram import filters
+from pyrogram.types import CallbackQuery, ChatPermissions, Message
+
+@app.on_message(filters.command("purge") & ~filters.edited & ~filters.private)
+@adminsOnly("can_delete_messages")
+async def purgeFunc(_, message: Message):
+    await message.delete()
+
+    if not message.reply_to_message:
+        return await message.reply_text("Reply to a message to purge from.")
+
+    chat_id = message.chat.id
+    message_ids = []
+
+    for message_id in range(
+        message.reply_to_message.message_id,
+        message.message_id,
+    ):
+        message_ids.append(message_id)
+
+        # Max message deletion limit is 4000
+        if len(message_ids) == 4000:
+            await app.delete_messages(
+                chat_id=chat_id,
+                message_ids=message_ids,
+                revoke=True,  # For both sides
+            )
+
+            # To delete more than 4000 messages, start again
+            message_ids = []
+
+    # Delete if any messages left
+    if len(message_ids) > 0:
+        await app.delete_messages(
+            chat_id=chat_id,
+            message_ids=message_ids,
+            revoke=True,
+        )
