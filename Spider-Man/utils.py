404077ee7641108669e315de93ce90117e80e3e2
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2022-02-13 09:07:15 +0530

    Update utils.py

--- Spider-Man/utils.py
+++ Spider-Man/utils.py
@@ -4,11 +4,11 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"Minato/plugins/{plugin_name}.py")
-    name = "Minato.plugins.{}".format(plugin_name)
+    path = Path(f"Spider-Man/Telethon/{plugin_name}.py")
+    name = "Spider-Man.Telethon.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["Minato.plugins." + plugin_name] = load
-    print("Minato has Imported " + plugin_name)
+    sys.modules["Spider-Man.Telethon." + plugin_name] = load
+    print("Spider Man has Imported " + plugin_name)
