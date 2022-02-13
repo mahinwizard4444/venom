b1f524311800ca9639247fdfd2aa3ec5b18aa207
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2022-02-13 09:17:13 +0530

    Update start.py

--- Spider-Man/Telethon/start.py
+++ Spider-Man/Telethon/start.py
@@ -1,1 +1,10 @@
-Start.()
+from .. import Bot
+from telethon import events, Button
+
+@Bot.on(events.NewMessage(incoming=True, pattern="/startbot"))
+async def startbot(event):
+    await event.reply("Telethon Working Fine ✅")
+
+@Bot.on(events.callbackquery.CallbackQuery(data="example"))
+async def ex(event):
+    await event.edit("Thanks For Using Bot")
