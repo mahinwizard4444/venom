99e514c88bb47817ad71209587935e4c980c5403
Mufaz-TG <93363575+BXBotz2021@users.noreply.github.com>
2022-05-01 11:48:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -467,7 +467,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     reply_markup=MOVIE_BTNSS)
         await query.message.reply(text=f"Hey 👋 {query.from_user.mention} 😍📫 Yᴏʀ Fɪʟᴇ ɪꜱ Rᴇᴀᴅʏ 👇\n📂 Mᴏᴠɪᴇ Nᴀᴍᴇ : {title}\n⚙️ Mᴏᴠɪᴇ Sɪᴢᴇ : {size}", reply_markup=GROUP_BUTTONS)
                 return
-
     elif query.data == "pages":
         await query.answer()
     elif query.data == "start":
